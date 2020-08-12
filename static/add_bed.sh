d8dea0e5992b7d08584d2df4e3cfbd4361153c45
Qishun Wang <qishunw@unc.edu>
2020-08-11 19:08:14 -0700

    Delete local file after add to give container

--- static/add_bed.sh
+++ static/add_bed.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 docker exec -it give /bin/bash -c "bash add_track_bed.sh -u root -p Admin2015 -r hg38 -t \"$1\" -g \"$2\" -l \"Null\" -s \"$3\" -o 1 -v full -f /mnt/my_data/$4"
+
+rm ~/GIVE_web/$4
